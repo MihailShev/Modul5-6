0fb80afcbbf0f6902f208726a226ad547eef6288
Mihail <mykh.shev4enko@gmail.com>
2024-12-07 12:49:58 +0200

    done

--- js/task-08.js
+++ js/task-08.js
@@ -1,3 +1,12 @@
 // Напишіть функцію для зберігання знижки.
 // Функція повертає іншу функцію, яка приймає суму покупки
 // та повертає фінальну суму зі збереженою знижкою
+
+function saleName(sale) {
+  return (sumBye) => sumBye - sumBye * (sale / 100);
+}
+
+const disc20 = saleName(20);
+const disk17 = saleName(17);
+console.log(disk17(30));
+console.log(disc20(30));
