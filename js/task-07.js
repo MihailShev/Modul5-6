3735ab2450265bf6f292e60c9c1d883bc19e698d
Dima Hirchyk <dimzuk04@gmail.com>
2024-12-07 12:22:09 +0200

    task7

--- js/task-07.js
+++ js/task-07.js
@@ -1,3 +1,12 @@
 // Напишіть функцію savePassword(password), яка приймає пароль та повертає внутрішню функцію,
 //     яка приймає рядок та повертає true, якщо рядок співпадає зі збереженим паролем,
 //     та false - якщо не співпадає
+
+function savePassword(password) {
+  return (newPass) => newPass === password;
+}
+
+const passwords = savePassword("qwerty");
+
+console.log(passwords("qwerty"));
+console.log(passwords("qwer"));
