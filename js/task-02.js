972a78bf34e818fd1e6da35240b3314fd05b0bc9
Art-Rud <poltavarud@gmail.com>
2024-12-07 11:32:23 +0200

    done

--- js/task-02.js
+++ js/task-02.js
@@ -3,3 +3,15 @@
 // Функція створює об"єкт товару, додає йому унікальний ідентифікатор у властивість id та викликає callback,
 // передаючи йому створений об"єкт
 // showProduct(product)  - колбек, що приймає об"єкт продукта та виводить його у консоль
+function makeProduct(name, price, callback) {
+  const product = {
+    name,
+    price,
+    id: Math.random(),
+  };
+  callback(product);
+}
+function showProduct(product) {
+  console.log(product);
+}
+makeProduct("apple", 100, showProduct);
